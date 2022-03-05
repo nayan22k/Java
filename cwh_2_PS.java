ab0479282eb8a16cfe4965d353a0b49dda08d0ad
nayan22k <76203030+nayan22k@users.noreply.github.com>
2022-03-05 23:26:29 +0530

    Add files via upload

---
+++ cwh_2_PS.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class cwh_2_PS {
+    public static void main(String[] args) {
+        // ques 1
+//        int a = 4;
+//        int b = 17;
+//        int c = 6;
+//        int sum = a + b + c;
+//        System.out.println(sum);
+
+        // ques 2
+    // float subject1 = 45;
+    //    float subject2 = 95;
+    //    float subject3 = 48;
+    //    float cgpa = (subject1 + subject2 + subject3)/30;
+    //    System.out.println(cgpa);
+
+        //ques 3
+//        System.out.println("What is your name");
+ //       Scanner sc = new Scanner(System.in);
+ //       String name = sc.next();
+//        System.out.println("Hello " + name + " have a good day!" );
+
+        //ques 4
+//       float km = 2;
+//       double result = (km*0.62);
+//        System.out.println(result);
+
+        //ques 5
+     //   System.out.println("Enter your number");
+        //   Scanner sc = new Scanner(System.in);
+     //  System.out.println(sc.hasNextInt());
+
+
+    }
+}
