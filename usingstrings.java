2c3489f6018349f5d56a99de26d94f58d269b05d
Nayan Koshta <nayankoshta22@gmail.com>
2022-03-07 23:40:59 +0530

    Add files via upload

---
+++ usingstrings.java
@@ -0,0 +1,18 @@
+package com.company;
+import java.util.Scanner;
+
+public class usingstrings {
+    public static void main(String[] args) {
+    //    String name = "Harry";
+    //    System.out.print("The name is : " );
+    //    System.out.print(name);
+
+    //    int a = 6;
+    //    float b = 5.645f;
+    //    System.out.printf("The value of a is %d and value of b is %f", a, b);
+    Scanner sc = new Scanner(System.in);
+//    String st = sc.next();
+    String st = sc.nextLine();
+        System.out.println(st);
+    }
+}
