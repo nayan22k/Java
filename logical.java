90759d334529edbbd348ccc154d821212907d465
Nayan Koshta <nayankoshta22@gmail.com>
2022-03-08 21:01:45 +0530

    Add files via upload

---
+++ logical.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class logical {
+    public static void main(String[] args) {
+        System.out.println("For logical AND...");
+        boolean a = true;
+        boolean b = false;
+    /*    boolean c = true;
+        if ( a && b && c){
+            System.out.println("Y");
+        }
+        else{
+            System.out.println("N");
+        } */
+    /*
+        System.out.println("For logical OR...");
+        boolean a = true;
+        boolean b = false;
+        if ( a || b){
+            System.out.println("Y");
+        }
+        else{
+            System.out.println("N");
+        } */
+
+        System.out.println("For logical NOT");
+        System.out.print("Not(a) is ");
+        System.out.println(!a);
+        System.out.print("Not(b) is ");
+        System.out.println((!b));
+
+    }
+}
