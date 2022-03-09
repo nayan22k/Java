9c268061b523d53e928003e34ce02244c9f4902a
Nayan Koshta <nayankoshta22@gmail.com>
2022-03-09 22:34:24 +0530

    Add files via upload

---
+++ PracticSet5.java
@@ -0,0 +1,61 @@
+package com.company;
+
+public class PracticSet5 {
+    public static void main(String[] args) {
+        // Problem 1
+        //    int n = 4;
+        //    for (int i=n ; i>0; i--){
+        //        for (int j=0; j<i;j++) {
+        //            System.out.print("*");
+        //        }
+        //        System.out.print("\n");
+
+        // Problem 2
+        //    int sum=0;
+        //    int n=4;
+        //    for (int i=0;i<n;i++){
+        //        sum = sum + (2*i);
+        //    }
+        //    System.out.println("Sum of even numbers is: ");
+        //    System.out.println(sum);
+
+        // Problem 3
+    //    int n = 5;
+        //for (int i=0; i<10; i++)
+    //    for (int i = 1; i <= 10; i++)
+    //    System.out.println(n+"X"+i+"="+n*i);
+
+        // Problem 4
+    //    int n = 5;
+    //        for (int i = 10; i >= 1; i--)
+    //        System.out.println(n+"X"+i+"="+n*i);
+
+        // Problem 6
+    /*    int n=5;
+        //what is factorial n = n*n-1*n-2....1
+        //5! = 5*4*3*2*1
+        int i = 1;
+        int factorial = 1;
+        while (i<=n) {
+            factorial *= i;
+            i++;
+        }
+        System.out.println(factorial);
+
+     */
+
+        // Problem 9
+    /*    int n = 8;
+        int sum = 0;
+        for (int i=1; i<=10; i++){
+            sum +=n*i;
+        }
+        System.out.println(sum);
+
+     */
+
+        }
+
+    }
+
+
