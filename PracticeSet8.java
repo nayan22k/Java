884016e59cc2681ad2665aaae37390fb410cf0d5
Nayan Koshta <nayankoshta22@gmail.com>
2022-03-12 22:09:06 +0530

    Add files via upload

---
+++ PracticeSet8.java
@@ -0,0 +1,81 @@
+package com.company;
+
+class Employee{
+    int salary;
+    String name;
+    public int getSalary(){
+        return salary;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String n){
+        name = n;
+    }
+}
+
+class CellPhone{
+    public void ringing(){
+        System.out.println("Ringing...");
+    }
+    public void vibrating(){
+        System.out.println("Vibrating...");
+    }
+    public void callFriend(){
+        System.out.println("Calling Mukul...");
+    }
+}
+
+class Square {
+    int side;
+    public int area(){
+        return side*side;
+    }
+    public int perimeter(){
+        return 4*side;
+    }
+}
+class Tommy{
+    public void hit(){
+        System.out.println("Hitting the enemy");
+    }
+    public void run(){
+        System.out.println("Running from the enemy");
+    }
+    public void fire(){
+        System.out.println("Firing on the enemy");
+    }
+}
+
+public class cwh_39_ps8 {
+    public static void main(String[] args) {
+        /*
+        // Problem 1
+        Employee harry = new Employee();
+        harry.setName("CodewithHarry");
+        harry.salary = 233;
+        System.out.println(harry.getSalary());
+        System.out.println(harry.getName());
+
+        // Problem 2
+
+        CellPhone asus = new CellPhone();
+        asus.callFriend();
+        asus.vibrating();
+        asus.ringing();
+
+        // Problem 3
+        Square sq = new Square();
+        sq.side = 3;
+        System.out.println(sq.area());
+        System.out.println(sq.perimeter());
+
+         */
+
+        // Problem 5
+        Tommy player1 = new Tommy();
+        player1.fire();
+        player1.run();
+        player1.hit();
+    }
+}
