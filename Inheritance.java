3ef95468d9c39282508483050f471e74d838e218
Nayan Koshta <nayankoshta22@gmail.com>
2022-03-13 23:53:50 +0530

    Add files via upload

---
+++ Inheritance.java
@@ -0,0 +1,46 @@
+package com.company;
+
+class Base{
+    int x;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        System.out.println("I am in base and setting X now");
+        this.x = x;
+    }
+
+    public void printMe(){
+        System.out.println("I am a constructor");
+    }
+}
+
+class Derived extends Base{
+    int y;
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
+
+public class cwh_45_Inheritance {
+    public static void main(String[] args) {
+        // Creating an object of Base class
+        Base b = new Base();
+        b.setX(4);
+        System.out.println(b.getX());
+
+        // Creating an object of derived class
+        Derived d = new Derived();
+        d.setY(43);
+        System.out.println(d.getY());
+
+
+    }
+}
