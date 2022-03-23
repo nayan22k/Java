8a191a43354914eb035af9f45be175d0c047a1b2
Nayan Koshta <nayankoshta22@gmail.com>
2022-03-23 22:46:10 +0530

    Add files via upload

---
+++ OutputFormatting.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.io.*;
+import java.util.*;
+
+    public class OutputFormatting {
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++){
+                String s = sc.next();
+                int x = sc.nextInt();
+                System.out.printf("%-15s%03d%n",s,x);
+            }
+            System.out.println("================================");
+
+
+
+        }
+    }
+
