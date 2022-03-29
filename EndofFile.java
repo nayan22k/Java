c3e1fe1e08246dbab45cce89fb1edbbc67edf2e6
Nayan Koshta <nayankoshta22@gmail.com>
2022-03-29 21:46:44 +0530

    Add files via upload

---
+++ EndofFile.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.io.*;
+import java.util.*;
+
+public class EndofFile {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int i = 0;
+        while(sc.hasNext()){
+            i++;
+            System.out.println(i + " " + sc.nextLine());
+        }
+    }
+}
