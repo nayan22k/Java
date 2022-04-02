15ea757a857e73ce98c089bbacf7c09c995df7f3
Nayan Koshta <nayankoshta22@gmail.com>
2022-04-02 20:22:20 +0530

    Add files via upload

---
+++ HRsubstring2.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.util.*;
+
+public class HRsubstring2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.print(s.substring(a, b));
+    }
+}
